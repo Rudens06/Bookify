136bb8f76c671b2d4e0f0a19024ac0dd70d88fc8
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-10-12 17:41:56 +0300

    Add context to token

--- lib/bookify/auth/token.ex
+++ lib/bookify/auth/token.ex
@@ -6,10 +6,12 @@ defmodule Bookify.Auth.Token do
 
   @cast_fields [:token, :expires_at, :user_id]
   @required_fields [:token, :expires_at, :user_id]
+  @token_contexts ["api", "remeber"]
 
   schema("tokens") do
     field :hashed_token, :string
     field :expires_at, :string
+    field :context, :string
     belongs_to :user, User
 
     timestamps(updated_at: false)
@@ -19,6 +21,7 @@ defmodule Bookify.Auth.Token do
     %__MODULE__{}
     |> cast(attrs, @cast_fields)
     |> validate_required(@required_fields)
+    |> validate_inclusion(:context, @token_contexts)
     |> unique_constraint(:token)
   end
 end
