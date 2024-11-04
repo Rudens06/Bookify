a9a73d3f41d3ec23b4d85c1b002d8efc5139199a
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-11-04 18:34:43 +0200

    Ensure list owner

--- lib/bookify_web/controllers/plugs/ensure_list_owner.ex
+++ lib/bookify_web/controllers/plugs/ensure_list_owner.ex
@@ -4,21 +4,27 @@ defmodule BookifyWeb.Plugs.EnsureListOwner do
   import Bookify.Utils.User
 
   alias Bookify.Lists
+  alias Bookify.Lists.List
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     list_id = conn.params["list_id"]
     user = current_user(conn)
-    list = Lists.get_list!(list_id)
 
-    if list.user_id == user.id do
-      assign(conn, :list, list)
-    else
-      conn
-      |> put_status(:forbidden)
-      |> json(%{error: "You are not authorized to access this list."})
-      |> halt()
+    case Lists.get_list(list_id) do
+      %List{} = list ->
+        if list.user_id == user.id do
+          assign(conn, :list, list)
+        else
+          conn
+          |> put_status(:forbidden)
+          |> json(%{error: "You are not authorized to access this list."})
+          |> halt()
+        end
+
+      error ->
+        error
     end
   end
 end
