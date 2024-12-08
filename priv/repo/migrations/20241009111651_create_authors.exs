9d5fc7d2aeae60df0ff6df0d794c3bb4bc36fa7c
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-12-08 02:20:52 +0200

    remove author death year

--- priv/repo/migrations/20241009111651_create_authors.exs
+++ priv/repo/migrations/20241009111651_create_authors.exs
@@ -4,9 +4,8 @@ defmodule Bookify.Repo.Migrations.CreateAuthors do
   def change do
     create table(:authors) do
       add :name, :string, null: false
-      add :birth_year, :integer
-      add :death_year, :integer
-      add :biography, :string
+      add :birth_year, :integer, null: false
+      add :biography, :string, null: false
       add :image_url, :string
       add :wikipedia_url, :string
 
