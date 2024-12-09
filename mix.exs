86e579ba9325b4cf6fe0acbd28d08be3f2c53cc1
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-12-09 14:02:57 +0200

    Add Tesla dep

--- mix.exs
+++ mix.exs
@@ -47,12 +47,14 @@ defmodule Bookify.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:puid, "~> 2.0"},
       {:bcrypt_elixir, "~> 3.0"},
       {:httpoison, "~> 2.0"},
-      {:floki, "~> 0.36.0"}
+      {:floki, "~> 0.36.0"},
+      {:tesla, "~> 1.11"},
+      {:jason, "~> 1.4"},
+      {:mint, "~> 1.0"}
     ]
   end
 
