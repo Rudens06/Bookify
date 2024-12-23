45b5eaeae236f290f1f1f749e41643a6b4f5ae77
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-12-23 18:13:13 +0200

    Add infinite scroll authors, books

--- lib/bookify_web/live/author_live/index.ex
+++ lib/bookify_web/live/author_live/index.ex
@@ -6,72 +6,145 @@ defmodule BookifyWeb.AuthorLive.Index do
   alias Bookify.Authors
   alias Bookify.Authors.Author
 
+  @page_size 18
+  @default_offset 0
+
   def mount(_params, _session, socket) do
-    authors = Authors.list_authors()
+    authors = Authors.list_authors(@page_size, @default_offset)
 
     {:ok,
      socket
-     |> assign(:authors, authors)
-     |> assign(:query, "")}
+     |> stream(:authors, authors)
+     |> assign(:query, "")
+     |> assign(:page_title, "Authors")
+     |> assign(:author, nil)
+     |> assign(:modal_action, nil)
+     |> assign(:page, 1)}
   end
 
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  def handle_params(_params, _url, socket) do
+    {:noreply, socket}
   end
 
   def handle_event("search", %{"value" => query}, socket) do
     authors =
       if query == "" do
-        Authors.list_authors()
+        Authors.list_authors(@page_size, @default_offset)
       else
-        Authors.search_authors(query)
+        Authors.search_authors(query, @page_size, @default_offset)
       end
 
     {:noreply,
      socket
-     |> assign(:authors, authors)
-     |> assign(:query, query)}
+     |> assign(:page, 1)
+     |> assign(:query, query)
+     |> stream(:authors, authors, reset: true)}
   end
 
   def handle_event("add_author", _params, socket) do
-    {:noreply, push_navigate(socket, to: ~p"/authors/new")}
-  end
+    user = current_user(socket)
 
-  def handle_event("edit_author", %{"id" => id}, socket) do
-    {:noreply, push_navigate(socket, to: ~p"/authors/#{id}/edit")}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    case validate_integer_id(id) do
-      {:ok, id} ->
-        case Authors.get_author(id) do
-          author = %Author{} ->
-            socket
-            |> assign(:page_title, "Edit Author")
-            |> assign(:author, author)
-
-          {:error, {:not_found, message}} ->
-            socket
-            |> put_flash(:error, message)
-            |> push_navigate(to: ~p"/authors")
-        end
-
-      {:error, _} ->
+    if is_admin?(user) do
+      socket =
         socket
-        |> put_flash(:error, "Author not found")
-        |> push_navigate(to: ~p"/authors")
+        |> assign(:page_title, "New Author")
+        |> assign(:author, %Author{})
+        |> assign(:modal_action, :new)
+
+      {:noreply, socket}
+    else
+      not_allowed(socket)
     end
   end
 
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Author")
-    |> assign(:author, %Author{})
+  def handle_event("edit_author", %{"id" => id}, socket) do
+    user = current_user(socket)
+
+    if is_admin?(user) do
+      socket =
+        case validate_integer_id(id) do
+          {:ok, id} ->
+            case Authors.get_author(id) do
+              author = %Author{} ->
+                socket
+                |> assign(:page_title, "Edit Author")
+                |> assign(:author, author)
+                |> assign(:modal_action, :edit)
+
+              {:error, {:not_found, message}} ->
+                socket
+                |> put_flash(:error, message)
+                |> push_navigate(to: ~p"/authors")
+            end
+
+          {:error, _} ->
+            socket
+            |> put_flash(:error, "Author not found")
+            |> push_navigate(to: ~p"/authors")
+        end
+
+      {:noreply, socket}
+    else
+      not_allowed(socket)
+    end
   end
 
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Authors")
-    |> assign(:author, nil)
+  def handle_event("delete_author", %{"id" => id}, socket) do
+    user = current_user(socket)
+
+    if is_admin?(user) do
+      author = Authors.get_author(id)
+
+      socket =
+        case Authors.delete_author(author) do
+          {:ok, _} ->
+            socket
+            |> stream_delete(:authors, author)
+            |> put_flash(:info, "Author deleted successfully")
+
+          {:error, _} ->
+            socket
+            |> put_flash(:error, "Something went wrong")
+        end
+
+      {:noreply, socket}
+    else
+      not_allowed(socket)
+    end
+  end
+
+  def handle_event("load_more", _params, socket) do
+    authors =
+      if socket.assigns.query == "" do
+        Authors.list_authors(@page_size, socket.assigns.page * @page_size)
+      else
+        Authors.search_authors(
+          socket.assigns.query,
+          @page_size,
+          socket.assigns.page * @page_size
+        )
+      end
+
+    {:noreply,
+     assign(socket, page: socket.assigns.page + 1)
+     |> stream(:authors, authors, at: -1)}
+  end
+
+  def handle_event("dismiss_modal", _params, socket) do
+    {:noreply, assign(socket, :modal_action, nil)}
+  end
+
+  def handle_info({:saved, author}, socket) do
+    {:noreply,
+     socket
+     |> assign(:modal_action, nil)
+     |> stream_insert(:authors, author)}
+  end
+
+  defp not_allowed(socket) do
+    {:noreply,
+     socket
+     |> put_flash(:error, "Not allowed!")
+     |> push_navigate(to: ~p"/authors")}
   end
 end
--- lib/bookify_web/live/book_live/index.ex
+++ lib/bookify_web/live/book_live/index.ex
@@ -7,76 +7,150 @@ defmodule BookifyWeb.BookLive.Index do
   alias Bookify.Authors
   alias Bookify.Books.Book
 
+  @page_size 18
+  @default_offset 0
+
   def mount(_params, _session, socket) do
-    books = Books.list_books([:author])
+    books = Books.list_books(@page_size, @default_offset, [:author])
 
     {:ok,
      socket
      |> assign(:query, "")
-     |> assign(:books, books)}
+     |> assign(:page, 1)
+     |> assign(:page_title, "Books")
+     |> assign(:book, nil)
+     |> assign(:modal_action, nil)
+     |> stream(:books, books)}
   end
 
-  def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  def handle_params(_params, _url, socket) do
+    {:noreply, socket}
   end
 
   def handle_event("search", %{"value" => query}, socket) do
     books =
       if query == "" do
-        Books.list_books([:author])
+        Books.list_books(@page_size, @default_offset, [:author])
       else
-        Books.search_books(query, [:author])
+        Books.search_books(query, @page_size, @default_offset, [:author])
       end
 
     {
       :noreply,
       socket
       |> assign(:query, query)
-      |> assign(:books, books)
+      |> assign(:page, 1)
+      |> stream(:books, books, reset: true)
     }
   end
 
   def handle_event("add_book", _params, socket) do
-    {:noreply, push_navigate(socket, to: ~p"/books/new")}
-  end
+    user = current_user(socket)
 
-  def handle_event("edit_book", %{"isbn" => isbn}, socket) do
-    {:noreply, push_navigate(socket, to: ~p"/books/#{isbn}/edit")}
-  end
+    if is_admin?(user) do
+      authors =
+        Authors.list_authors()
+        |> select_options()
 
-  defp apply_action(socket, :edit, %{"isbn" => isbn}) do
-    case Books.get_book_by_isbn(isbn) do
-      book = %Book{} ->
-        authors =
-          Authors.list_authors()
-          |> select_options()
-
+      socket =
         socket
-        |> assign(:page_title, "Edit Book")
-        |> assign(:book, book)
+        |> assign(:page_title, "New Book")
+        |> assign(:book, %Book{})
+        |> assign(:modal_action, :new)
         |> assign(:authors, authors)
 
-      {:error, {:not_found, message}} ->
-        socket
-        |> put_flash(:error, message)
-        |> push_navigate(to: ~p"/")
+      {:noreply, socket}
+    else
+      not_allowed(socket)
     end
   end
 
-  defp apply_action(socket, :new, _params) do
-    authors =
-      Authors.list_authors()
-      |> select_options()
+  def handle_event("edit_book", %{"isbn" => isbn}, socket) do
+    user = current_user(socket)
 
-    socket
-    |> assign(:page_title, "New Book")
-    |> assign(:book, %Book{})
-    |> assign(:authors, authors)
+    if is_admin?(user) do
+      socket =
+        case Books.get_book_by_isbn(isbn) do
+          book = %Book{} ->
+            authors =
+              Authors.list_authors()
+              |> select_options()
+
+            socket
+            |> assign(:page_title, "Edit Book")
+            |> assign(:book, book)
+            |> assign(:modal_action, :edit)
+            |> assign(:authors, authors)
+
+          {:error, {:not_found, message}} ->
+            socket
+            |> put_flash(:error, message)
+            |> push_navigate(to: ~p"/")
+        end
+
+      {:noreply, socket}
+    else
+      not_allowed(socket)
+    end
   end
 
-  defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(:page_title, "Books")
-    |> assign(:book, nil)
+  def handle_event("delete_book", %{"isbn" => isbn}, socket) do
+    user = current_user(socket)
+
+    if is_admin?(user) do
+      book = Books.get_book_by_isbn(isbn)
+
+      socket =
+        case Books.delete_book(book) do
+          {:ok, _} ->
+            socket
+            |> stream_delete(:books, book)
+            |> put_flash(:info, "Book deleted successfully")
+
+          {:error, _} ->
+            socket
+            |> put_flash(:error, "Something went wrong")
+        end
+
+      {:noreply, socket}
+    else
+      not_allowed(socket)
+    end
+  end
+
+  def handle_event("dismiss_modal", _params, socket) do
+    {:noreply, assign(socket, :modal_action, nil)}
+  end
+
+  def handle_event("load_more", _params, socket) do
+    books =
+      if socket.assigns.query == "" do
+        Books.list_books(@page_size, socket.assigns.page * @page_size, [:author])
+      else
+        Books.search_books(
+          socket.assigns.query,
+          @page_size,
+          socket.assigns.page * @page_size,
+          [:author]
+        )
+      end
+
+    {:noreply,
+     assign(socket, page: socket.assigns.page + 1)
+     |> stream(:books, books, at: -1)}
+  end
+
+  def handle_info({:saved, book}, socket) do
+    {:noreply,
+     socket
+     |> assign(:modal_action, nil)
+     |> stream_insert(:books, book)}
+  end
+
+  defp not_allowed(socket) do
+    {:noreply,
+     socket
+     |> put_flash(:error, "Not allowed!")
+     |> push_navigate(to: ~p"/")}
   end
 end
