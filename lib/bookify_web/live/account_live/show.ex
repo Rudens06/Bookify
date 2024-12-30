cfa6ea45bb3f77063e23f90e33f4e1e9055def96
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-12-31 01:52:10 +0200

    Add messages for API token generation

--- lib/bookify_web/live/account_live/show.ex
+++ lib/bookify_web/live/account_live/show.ex
@@ -38,18 +38,27 @@ defmodule BookifyWeb.AccountLive.Show do
       current_user(socket)
       |> Users.generate_new_api_token()
 
-    {:noreply, stream_insert(socket, :api_tokens, user_token, at: 0)}
+    {:noreply,
+    socket
+    |> put_flash(:info, "New API token generated.")
+    |> stream_insert(:api_tokens, user_token, at: 0)}
   end
 
   def handle_event("delete_all_tokens", _params, socket) do
     Users.invalidate_tokens(current_user(socket), :api)
-    {:noreply, stream(socket, :api_tokens, [], reset: true)}
+    {:noreply,
+    socket
+    |> put_flash(:info, "All API tokens deleted.")
+    |> stream(:api_tokens, [], reset: true)}
   end
 
   def handle_event("delete_token", %{"id" => id}, socket) do
     token_id = String.to_integer(id)
     user_token = Users.get_token_by_id(current_user(socket), token_id)
     Users.invalidate_token(user_token.token)
-    {:noreply, stream_delete(socket, :api_tokens, user_token)}
+    {:noreply,
+    socket
+    |> put_flash(:info, "API token deleted.")
+    |> stream_delete(:api_tokens, user_token)}
   end
 end
